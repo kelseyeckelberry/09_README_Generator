7a4748e057bfa0a9565cf148ee4d315d5e9ab7bf
kelseck <kelseyeckelberry@gmail.com>
2020-11-16 22:28:01 -0600

    README Generator functions

---
+++ README.md
@@ -0,0 +1,36 @@
+![badge](https://img.shields.io/badge/license-MIT-blue)
+
+# Best Project
+
+## Description
+This is a test to demostrate functionality
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+To install necessary dependencies, run the following command.
+* npm i
+
+
+## Usage
+This is a test
+
+## License
+This project is licensed under the MIT license.
+
+## Contributing
+Please don't
+
+## Tests
+To run tests, run the following command.
+* npm test
+
+## Questions
+If you have any questions about the repo, open an issue or contact me directly at [email@gmail.com](email@gmail.com). You can find more of my work at [kelsey](https://github.com/kelsey). 
+
--- index.js
+++ index.js
@@ -9,22 +9,88 @@ const promptUser = () =>
       {
         type: "input",
         name: "title",
-        message: "What is the title of your project?",
+        message: "What is your project's name?",
       },
       {
         type: "input",
         name: "description",
-        message: "What is a description of your project?",
+        message: "Please write a short description of your project.",
       },
       {
-        type: "choices",
-        name: "toc",
-        message: "Would you like to include a Table of Contents?", choices: ["Yes", new inquirer.Separator(), "No"],
+        type: "input",
+        name: "install",
+        message: "What command should be run to install dependencies?",
+      },
+      {
+        type: "input",
+        name: "tests",
+        message: "What command should be run to run tests?",
+      },
+      {
+        type: "input",
+        name: "usage",
+        message: "What does the user need to know about using the repo?",
+      },
+      {
+        type: "input",
+        name: "contribute",
+        message: "What does the user need to know about contributing to the repo?",
+      },
+      {
+        type: "input",
+        name: "license",
+        message: "What kind of license should your project have?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is your email address?",
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "What is your GitHub username?",
       },
 ]);
 
 const generateREADME = (answers) =>
-``;
+`![badge](https://img.shields.io/badge/license-${answers.license}-blue)
+
+# ${answers.title}
+
+## Description
+${answers.description}
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+To install necessary dependencies, run the following command.
+* ${answers.install}
+
+
+## Usage
+${answers.usage}
+
+## License
+This project is licensed under the ${answers.license} license.
+
+## Contributing
+${answers.contribute}
+
+## Tests
+To run tests, run the following command.
+* ${answers.tests}
+
+## Questions
+If you have any questions about the repo, open an issue or contact me directly at [${answers.email}](${answers.email}). You can find more of my work at [${answers.github}](https://github.com/${answers.github}). 
+
+`;
 
 promptUser()
     .then((answers) => writeFileAsync("README.md", generateREADME(answers)))
